362fbb9d3ed6f7a8462ab1c690af018a8a5ef2f9
suryanshu pandit <suryanshup3@gmail.com>
2026-03-12 17:10:37 +0530

    Add files via upload
    exception handeling  is added on 12-03-2026

---
+++ T&P Adv-python/exception_handeling/banking_system&ta_ransaction.py
@@ -0,0 +1,18 @@
+class InsufficientFundsError(Exception): pass
+
+def process_transaction(balance, amount, target_account):
+    try:
+        if amount > balance:
+            raise InsufficientFundsError("Overdraft attempted.") 
+        if len(target_account) != 8:
+            raise ValueError("Incorrect account number format.") 
+        # Simulating a timeout
+        timeout = True 
+        if timeout:
+            raise ConnectionError("Transaction timed out. Please try again.") 
+    except InsufficientFundsError as e:
+        print(f"Bank Error: {e}")
+    except (ValueError, ConnectionError) as e:
+        print(f"System Error: {e}")
+
+process_transaction(100, 500, "123")
